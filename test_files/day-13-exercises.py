41447fc69831414e5dcf24fdc6b221066bf5e61e
Julian Charles <53817791+fancybaby404@users.noreply.github.com>
2021-05-22 17:49:47 +0800

    day-13-complete

---
+++ test_files/day-13-exercises.py
@@ -0,0 +1,30 @@
+# ------------ EXERCISE 1 <OddOrEven> ------------
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+# ------------ EXERCISE 2 <LeapYear> ------------
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+# ------------ EXERCISE 3 <FizzBuzz> ------------
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
